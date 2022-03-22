23afe3f36788a16e10455da5969742f5ba250711
AndrewPautov <apautov04@gmail.com>
2022-03-22 17:03:23 +0300

    Initial Commit

---
+++ GCD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Андрей Паутов on 22.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
